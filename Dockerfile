c8ba89a1955deea59968c1cb1074204efacdba4f
igallion <82122432+igallion@users.noreply.github.com>
2025-07-17 12:32:11 -0400

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+# syntax=docker/dockerfile:1
+
+FROM python:3.12.2-slim-bullseye
+
+WORKDIR /sql-app
+
+EXPOSE 80
+
+COPY requirements.txt /sql-app/
+
+RUN apt-get update && \
+    apt-get install -y wget unzip curl gnupg python3-dev build-essential libpcre3 libpcre3-dev unixodbc unixodbc-dev g++ \
+    && curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc \
+    && curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list | tee /etc/apt/sources.list.d/mssql-release.list \
+    && apt-get update \
+    && ACCEPT_EULA=Y apt-get install -y msodbcsql18
+
+RUN pip3 install --no-cache -r requirements.txt
+
+RUN wget https://releases.hashicorp.com/vault/1.20.0/vault_1.20.0_linux_amd64.zip \
+    && unzip -o vault_1.20.0_linux_amd64.zip -d /usr/local/bin/
+
+COPY app.py /sql-app/
+
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ app.py
@@ -0,0 +1,81 @@
+import pyodbc
+import os
+import time
+from fastapi import FastAPI, Response, status, HTTPException
+import hvac
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+#Connects to SQL DB and retrieves information from location table
+def connect_sql(USERNAME, PASSWORD, SERVER, DATABASE):
+    logger.info("Connecting to database with credentials:")
+    logger.info(f"Database Server {SERVER}:{DATABASE}")
+    logger.info(f"Username: {USERNAME} - Password: {PASSWORD}")
+
+    connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};DATABASE={DATABASE};SERVER={SERVER};UID={USERNAME};PWD={PASSWORD};Encrypt=no'
+    logger.info(f"Connection String: {connectionString}")
+    conn = pyodbc.connect(connectionString)
+    cursor = conn.cursor()
+    SQL_QUERY = "SELECT * FROM location"
+    logger.info(f"Running query: {SQL_QUERY}")
+    cursor.execute(SQL_QUERY)
+    resp = cursor.fetchall()
+
+    logger.info("Closing connection to database")
+    cursor.close()
+    conn.close()
+    return resp
+
+#Main SQL app
+def sql_app():
+    roleName = "mssql-role"
+    db_mount_point = 'database'
+    logger.info("Requesting database credentials from Vault")
+    creds = vault_client.secrets.database.generate_credentials(
+        name = roleName,
+        mount_point = db_mount_point
+    )
+    
+    logger.info("Requesting DB Server and Database name from Vault")
+    dbInfo = vault_client.secrets.kv.v1.read_secret(
+        path = 'sql-app/dev/dbinfo',
+        mount_point = 'BusinessUnit1'
+    )
+    logger.info(f"DB info: Server {dbInfo['data']['db_server']} Database Name: {dbInfo['data']['db_database']}")
+
+    logger.info("######### Connection #############")
+    try:
+        resp1 = connect_sql(creds['data']['username'], creds['data']['password'], dbInfo['data']['db_server'], dbInfo['data']['db_database'])
+    except pyodbc.InterfaceError as e:
+        logger.error(f"Failed to connect to database: {str(e)}")
+    return resp1
+
+app = FastAPI()
+
+#Initialize connection to Vault
+VAULT_ADDR = os.getenv('VAULT_ADDR')
+VAULT_USER = os.getenv('VAULT_USER')
+VAULT_PASS = os.getenv('VAULT_PASS')
+
+vault_client = hvac.Client(
+    url = VAULT_ADDR
+)
+
+vault_client.auth.userpass.login(
+    username = VAULT_USER,
+    password = VAULT_PASS
+)
+
+#Define api routes
+@app.get("/")
+def read_root():
+    logger.info("Returning message")
+    return {"Message": "Hello World"}
+
+@app.get("/sql-app", status_code=200)
+def get_sql_app(response: Response):
+    resp = sql_app()
+    logger.info(f"Returning response: {resp}")
+    return {"message": f"{resp}"}
